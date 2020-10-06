7c37e991cc7288329759d64a88f5b88847c1a88d
jramir93 <jramir93@uic.edu>
2020-10-05 22:18:51 -0500

    create gameboard table and set up initial ideas for logic

--- game.js
+++ game.js
@@ -1,13 +1,28 @@
-let playerOne = 'x';
-let playerTwo = 'y';
+let playerOne = 1;
+let playerTwo = 2;
 
-let gameBoard = [
-    ['_', '_', '_', '_', '_', '_', '_'],
-    ['_', '_', '_', '_', '_', '_', '_'],
-    ['_', 'l', 'o', 'l', '_', '_', '_'],
-    ['_', '_', '_', '_', '_', '_', '_'],
-    ['_', '_', '_', '_', '_', '_', '_'],
-    ['_', '_', '_', '_', '_', '_', '_']
-];
+let row = 6;
+let col = 7;
+let gameBoard;
 
-console.log(gameBoard);
+// //create the blank board for the game
+function createBlankBoard(){
+    gameBoard = new Array(6).fill(0).map(() => new Array(7).fill(0));
+}
+
+// make move
+function makeMove(input){
+
+}
+
+// check for winning condition
+function isWinner(){
+    
+}
+
+createBlankBoard();
+console.table(gameBoard);
+
+//while loop for the game
+
+console.log("fin");
